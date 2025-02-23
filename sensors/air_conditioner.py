33cddc82c7964a406d29be67c2d8cf193f6210b4
MiguelMaciel <miguemaciel@alu.ufc.br>
2025-02-23 12:06:39 -0300

    Atualização no ligar e desligar

--- sensors/air_conditioner.py
+++ sensors/air_conditioner.py
@@ -10,7 +10,7 @@ COMMAND_TOPIC = "temperature_commands"
 BROKER_URL = "localhost:9092"
 consumer =  KafkaConsumer(COMMAND_TOPIC, bootstrap_servers=BROKER_URL,
                          value_deserializer=lambda x: json.loads(x.decode('utf-8')))
-temperature = None
+temperature = 22
 power = "on"
 mode = "COOL"
 fan_speed = "MEDIUM"
@@ -22,12 +22,8 @@ producer = get_broker_connection()
 TOPIC = "temperature_data"
 
 def read_temperature():
-    """Simula a leitura de um sensor de temperatura"""
-    if temperature is not None:
-        # Simula influência da temperatura desejada (exemplo simples)
-        temp = temperature + random.uniform(-2, 2)  # Variação de +/- 2 graus
-    else:
-        temp = random.uniform(20.0, 30.0)
+    # Simula influência da temperatura desejada (exemplo simples)
+    temp = temperature + random.uniform(-2, 2)  # Variação de +/- 2 graus
     return int(round(temp, 0))
 
 def receive_commands():
@@ -36,13 +32,14 @@ def receive_commands():
         command = message.value
         if command.get("action") == "set_temperature":
             temperature = command.get("temperature")
-            print(f"[Temperature Sensor] Received command: {command}")
         if command.get("action") == "set_power":
             power = command.get("power")
             if power:
                 power = "on"
+                print("Power On")
             else:
                 power = "off"
+                print("Power Off")
         if command.get("action") == "set_mode":
             mode = command.get("mode")
             match mode:
@@ -51,7 +48,6 @@ def receive_commands():
                 case 2: mode = "FAN"
                 case 3: mode = "DRY"
                 case 4: mode = "AUTO"
-            print(f"[Temperature Sensor] Received command: {command}")
         if command.get("action") == "set_fan_speed":
             fan_speed = command.get("fan_speed")
             match fan_speed:
@@ -59,28 +55,11 @@ def receive_commands():
                 case 1: fan_speed = "MEDIUM"
                 case 2: fan_speed = "HIGH"
                 case 3: fan_speed = "AUTOMATIC"
-            print(f"[Temperature Sensor] Received command: {command}")
         if command.get("action") == "set_swing":
             swing = command.get("swing")
             match swing:
                 case True: swing = "True"
                 case False: swing = "False"
-            print(f"[Temperature Sensor] Received command: {command}")
-        
-        '''enum Mode {
-            COOL = 0;
-            HEAT = 1;
-            FAN = 2;
-            DRY = 3;
-            AUTO = 4;
-            }
-
-            enum FanSpeed {
-            LOW = 0;
-            MEDIUM = 1;
-            HIGH = 2;
-            AUTOMATIC = 3;
-            }'''
 
 command_thread = threading.Thread(target=receive_commands)
 command_thread.daemon = True
@@ -99,7 +78,8 @@ while True:
         }
     
     # Publicar no broker Kafka
-    producer.send(TOPIC, json.dumps(message).encode("utf-8"))
-    print(f"[AC Sensor] Sent: {message}")
+    if power == "on":
+        producer.send(TOPIC, json.dumps(message).encode("utf-8"))
+        print(f"[AC Sensor] Sent: {message}")
     
     time.sleep(15)  # Publica a cada 15 segundos
